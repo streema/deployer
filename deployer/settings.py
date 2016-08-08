976a78e7574d5fe39960a716fff8a4dfc6521f0c
Santiago Del Castillo <delcas@delcas.local>
2016-08-08 18:12:38 -0300

    use apps directory instead of streema

--- deployer/settings.py
+++ deployer/settings.py
@@ -7,5 +7,5 @@ env.forward_agent = True
 env.python_version = environ.get('APP_PYTHON_VERSION')
 
 env.app_name = environ.get('APP_NAME')
-env.app_dir = '/var/local/streema/{}'.format(environ.get('APP_NAME'))
+env.app_dir = '/var/local/apps/{}'.format(environ.get('APP_NAME'))
 env.repo_url = environ.get('APP_REPO_URL')
