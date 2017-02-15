0af7f9ab048690f9a32f3bb0825b67169a2aaa6e
Santiago Del Castillo <delcas@delcas.local>
2017-02-15 17:10:20 -0300

    Add django migrations task

--- deployer/tasks/django.py
+++ deployer/tasks/django.py
@@ -1,4 +1,4 @@
-from fabric.api import run
+from fabric.api import run, runs_once
 from fabric.decorators import with_settings
 
 from fabtools import require
@@ -8,3 +8,9 @@ from fabtools.python import virtualenv
 def collectstatic(app_name='', python_version=''):
     with virtualenv('/home/deploy/.pyenv/versions/{0}-{1}'.format(app_name, python_version)):
         run('python manage.py collectstatic -v0 --noinput')
+
+@runs_once
+@with_settings(warn_only=True)
+def migrate(app_name='', python_version=''):
+    with virtualenv('/home/deploy/.pyenv/versions/{0}-{1}'.format(app_name, python_version)):
+        run('python manage.py migrate')
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ install_reqs = parse_requirements(os.path.join(os.path.dirname(__file__), "requi
 
 setup(
     name="deployer",
-    version="1.2.3",
+    version="1.2.4",
     description="Streema deployer library",
     long_description=readme,
     author="Streema Devs",
